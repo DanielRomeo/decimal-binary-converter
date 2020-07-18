07487e4efa7f03f43a005c7d61bba464de94c3b5
Daniel Mamphekgo <Danielromeo99@gmail.com>
2020-07-18 20:05:58 +0200

    to decimal  left

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 // Author      : daniel
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : A basic number converter application
 //============================================================================
 
 #include <iostream>
@@ -21,7 +21,7 @@
 // using namespace std;
 
 /* a little reminder of things*/
-// std::cout << "Type of digit is : " << typeid(name).name() << std::endl;
+// std::cout << "Type of digit is : " << typeid(a).name() << std::endl;
 
 /*  loop through vector
 for(auto i = result.begin() ; i < result.end(); i++){
@@ -158,9 +158,9 @@ std::string decimalToHex(){
 	int i = 0;
 	float number;
 	int newNumber;
-	float base = 8.00;
+	float base = 16.00;
 	int mainer;
-	float answer = 0.00;
+	float answer = 1.00;
 	// float remainder;
 	float decimal ;
 	int digit;
@@ -170,77 +170,62 @@ std::string decimalToHex(){
 	std::string sDigit ;
 	std::string result;
 
-	std::cout << "input the number you want to convert to Decimal" << std::endl;
+	std::cout << "input the number you want to convert to hexidecimal" << std::endl;
 	std::cin >> number;
 
-
+	mainer = 1;
 	answer = number / base;
-	while(answer >0){
-
-		//may need to figure out why answer never goes below zero.
-		if(answer == 0){
-			i++;
-		}
-		if(i == 5){
-			break;
-		}
-
+	while(mainer !=0){
 		mainer = (int)answer;
+		// std::cout << mainer << "<- ";
 		decimal = answer - mainer;
 		digit = base * decimal;
 
 		// convert digit to string:
 		sDigit = std::to_string(digit);
 		//Hex adjustments-- convert numbers to Alphabets
-
-		// switch(sDigit){
-		// 	case "10": 
-		// 		charector = "A";
-		// 		bin.push_back(charector);
-		// 		continue;
-		// 	case "11": 
-		// 		charector = "B";
-		// 		bin.push_back(charector);
-		// 		continue;
-		// 	case "12": 
-		// 		charector = "C";
-		// 		bin.push_back(charector);
-		// 		continue;
-		// 	case "13": 
-		// 		charector = "D";
-		// 		bin.push_back(charector);
-		// 		continue;
-		// 	case "14": 
-		// 		charector = "E";
-		// 		bin.push_back(charector);
-		// 		continue;
-		// 	case "15": 
-		// 		charector = "F";
-		// 		bin.push_back(charector);
-		// 		continue;
-		// }
+		// std::cout << " digit: " << sDigit << " ";
 		if(sDigit == "10"){
 			charector = "A";
 			bin.push_back("A");
-			continue;
+			// continue;
+		}else if(sDigit == "11"){
+			charector = "B";
+			bin.push_back("B");
+			// continue;
+		}else if(sDigit == "12"){
+			charector = "C";
+			bin.push_back("C");
+			// continue;
+		}else if(sDigit == "13"){
+			charector = "D";
+			bin.push_back("D");
+			// continue;
+		}else if(sDigit == "14"){
+			charector = "E";
+			bin.push_back("E");
+			// continue;
+		}else if(sDigit == "15"){
+			charector = "F";
+			bin.push_back("F");
+			// continue;
+		}else{
+			bin.push_back(sDigit);	
 		}
 
-
-		bin.push_back(sDigit);
-
-
-
-		number = answer;	
+		number = mainer;	
 		answer = number / base;
+
 	}
+
+	// reverse a vector:
+	std::reverse(std::begin(bin), std::end(bin));
 
 	//convert vector to a string:
 	for(const auto &piece : bin){
 		result += piece;
 	}
-
-	// newNumber = vectorToIntegerConverter(bin);
-	return "a";	
+	return result;
 }
 
 /*-----------------------------*/
@@ -258,7 +243,7 @@ int decimalToOct(){
 	int sum = 0;
 	std::vector<int> bin;
 
-	std::cout << "input the number you want to convert to Decimal" << std::endl;
+	std::cout << "input the number you want to convert to Octal" << std::endl;
 	std::cin >> number;
 
 	//newNumber = number;
@@ -287,14 +272,94 @@ int decimalToOct(){
 	return newNumber;	
 }
 
+/*hexidecimal to octal*/
+int hexToDecimal(){
+	// int i = 0;
+	std::string number;
+	int thePower;
+	int base = 16;
+
+	float answer = 0.00;
+	// float remainder;
+
+	int digit;
+	int sum = 0;
+	std::vector<std::string> bin;
+
+	std::cout << "input the Hex number you want to convert to decimal" << std::endl;
+	std::cin >> number;
+
+	// loop through the string:
+	for(std::string::size_type i = 0; i < number.size(); ++i){
+
+		// convert a number[i] into a string 
+		std::stringstream ss;
+		ss << number[i];
+		std::string str = ss.str();
+
+		if(str == "a" || str == "A"){
+			bin.push_back("10");
+			continue;			
+		}else if(str == "b" || str == "B"){
+			bin.push_back("11");
+			continue;			
+		}else if(str == "c" || str == "C"){
+			bin.push_back("12");
+			continue;			
+		}else if(str == "d" || str == "D"){
+			bin.push_back("13");
+			continue;			
+		}else if(str == "e" || str == "E"){
+			bin.push_back("14");
+			continue;			
+		}else if(str == "f" || str == "F"){
+			bin.push_back("15");
+			continue;			
+		}
+		bin.push_back(str);
+	}
+
+	std::string ab;
+	// now .. i have a vector of integers:
+	// loop through vector:
+
+
+	thePower = bin.size();
+	for (std::string::size_type i = 0; i < bin.size(); ++i)
+	{
+		std::stringstream ss;
+		ss << bin[i];
+		std::string str2 = ss.str();
+
+		digit = str2[i] - '0';
+
+
+		//std::cout << digit;
+		thePower--;
+		digit = digit *pow(base, thePower) ;
+		sum += digit;
+	}
+
+	return sum;
+
+}
+
+/*octal to decimal*/
+int octToDecimal(){
+
+}
+
+
+
 // -------------------------------------------------------------------------------------------
 
 int main() {
 	char input;
 	auto output= 0.00;
-
+	std::string outta;
 	std::cout << "(A) Convert binary to decimal \n---------------------- \n(B) Convert decimal to binary\n(C) Convert decimal to hexidecimal \n(D) Convert decimal to Octal" << std::endl;
-	
+	std::cout << "(E) Convert hexidecimal to decimal" << std::endl;
+	std::cout << "(F) Convert octal to decimal" << std::endl;
 
 	std::cin >> input;
 	if(input == 'a' || input =='A'){
@@ -302,12 +367,21 @@ int main() {
 	}else if(input == 'b' || input == 'B'){
 		output = decimalToBinary();
 	}else if(input == 'c' || input == 'C'){
-		output = decimalToHex();
+		outta = decimalToHex();
 	}else if(input == 'd' || input == 'D'){
 		output = decimalToOct();
+	}else if(input == 'e' || input == 'E'){
+		output = hexToDecimal();
+	}else if(input == 'f' || input == 'F'){
+		output = octToDecimal();
 	}
 
-	std::cout <<"\nAnswer : " <<  output << std::endl;
+	if(output = 0.00){
+		std::cout << outta;
+	}else{
+		std::cout <<"\nAnswer : " <<  output << std::endl;	
+	}
+	
 
 	return 0;
 }
